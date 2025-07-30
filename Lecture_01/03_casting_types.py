66a9b6ce02a02c90fa74bb39801ad472b3a4e9b7
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-07-30 00:58:00 -0600

    feat(lesson 1): variables and types

---
+++ Lecture_01/03_casting_types.py
@@ -0,0 +1,13 @@
+print("Type casting");
+
+""""""
+
+print(type(float("3.1515")));
+print(type(int(float("3.1515"))));
+
+
+print(bool(0));
+print(bool(4));
+print(bool(156));
+print(bool("0"));
+print(bool(""));
