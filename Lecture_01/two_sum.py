1b0b2997f4954a71be39164ad2a071ecaba4cada
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-08-07 02:04:29 -0600

    feat(day 3): second test

--- Lecture_01/two_sum.py
+++ Lecture_01/two_sum.py
@@ -1,20 +1,18 @@
 def two_sum(number_array, target):
   ## 1. example input: [2, 9, 7, 10]; target = 11;
   indeces = [];
-  for i in range(len(number_array) - 1):
+  for i in range(len(number_array)):
     current_number = number_array[i];
     print(current_number);
-    for j in range(len(number_array) - 1):
-      next_number = number_array[j+1]
+    for j in range(i+1,len(number_array)):
+      next_number = number_array[j]
       print(next_number);
       if (current_number + next_number == target):
         indeces.append(i);
-        indeces.append(j+1);
+        indeces.append(j);
         return indeces;
   if len(indeces) == 0:
     return "Not possible solution given the input array!"
   
-
-
-print(two_sum([3,2,4], 6));
+ 
 
